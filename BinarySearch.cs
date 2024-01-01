8c8d464516bf482d6c0fcecd0cac64582c13a1d2
Mahmoud <sherbeny2096@gmail.com>
2024-01-01 17:33:08 +0300

    first commit

---
+++ BinarySearch.cs
@@ -0,0 +1,25 @@
+﻿namespace grokking;
+public class BinarySearch
+{
+	public int binarySearch(int[] arrList, int item)
+	{
+		int min = 0;
+		int max = arrList.Length - 1;
+
+		while (min <= max)
+		{
+			int mid = (min + max) / 2;
+			int midItem = arrList[mid];
+
+			if (midItem == item)
+				return mid;
+			else if (item > midItem)
+				min = mid + 1;
+			else
+				max = mid - 1;
+		}
+
+		return -1;
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿using grokking;
+
+BinarySearch binarySearch = new BinarySearch();
+int[] arr = { 1, 3, 5, 7, 9, 13 };
+Console.WriteLine(binarySearch.binarySearch(arr, 10));
